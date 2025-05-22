06c84a80b64744b5ec1ff2361aaa4ed2491abee0
邵前前 <>
2025-05-22 16:11:22 +0800

    AdMoreMAX

--- pod_trunk.sh
+++ pod_trunk.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "AdMoreMAX"
 git push origin main
-git tag 3.4.4
+git tag 3.4.5
 git push origin main --tags
 pod trunk push AdMoreMAX.podspec --skip-import-validation --skip-tests --allow-warnings
